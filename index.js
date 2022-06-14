21cf8be511fb092875bbf8699441645c782cc50b
Roni Koskinen <roni.koskinen@gmail.com>
2022-06-14 14:39:03 +0300

    2

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
 
 let persons = [
-      { 
-        "name": "Arto Hellas", 
-        "number": "040-123456",
-        "id": 1
-      },
-      { 
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      { 
-        "name": "Dan Abramov", 
-        "number": "12-43-234345",
-        "id": 3
-      },
-      { 
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122",
-        "id": 4
-      }
-    ]
+{ 
+  "name": "Arto Hellas", 
+  "number": "040-123456",
+  "id": 1
+},
+{ 
+  "name": "Ada Lovelace", 
+  "number": "39-44-5323523",
+  "id": 2
+},
+{ 
+  "name": "Dan Abramov", 
+  "number": "12-43-234345",
+  "id": 3
+},
+{ 
+  "name": "Mary Poppendieck", 
+  "number": "39-23-6423122",
+  "id": 4
+}
+]
 
+
+app.use(express.json())
 morgan.token('body', (req)=> JSON.stringify(req.body))
 
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body`, 
 {skip: (req,res)=> req.method !== "POST"}))
 
 app.use(morgan('tiny', {skip: (req,res)=> req.method === "POST"}))
+app.use(cors())
 
-app.use(express.json())
+app.use(express.static('build'))
+
+
 
 const generateId = () => {
     const maxId = persons.length > 0
@@ -101,8 +107,13 @@ app.get('/api/persons/:id', (request, response) => {
     response.json(person)
   })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-const PORT = 3001
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
