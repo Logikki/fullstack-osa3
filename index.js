88aaeab1cb19a562bd8c7ae8bdebb186f3c776ab
Roni Koskinen <roni.koskinen@gmail.com>
2022-06-20 13:23:18 +0300

    uibuild

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const morgan = require('morgan')
 require('dotenv').config()
 const Person = require('./models/person')
+const { query } = require('express')
 
 app.use(express.json())
 morgan.token('body', (req)=> JSON.stringify(req.body))
@@ -64,7 +65,7 @@ app.get('/api/persons/:id', (request, response, next) => {
     })
   })
 
-  app.post('/api/persons', (request, response) => {
+  app.post('/api/persons', (request, response, next) => {
     const body = request.body
   
     if (!body.name || !body.number) {
@@ -80,21 +81,23 @@ app.get('/api/persons/:id', (request, response, next) => {
      number : body.number
     })
   
-    person.save().then(savedPerson =>{
+    person.save()
+    .then(savedPerson =>{
       response.json(savedPerson)
       console.log(savedPerson)
     })
+    .catch(error => next(error))
   })
 
   app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const {name, number } = request.body
+    // const person = {
+    //   name, number
+    // }
   
-    const person = {
-      name: body.name,
-      number: body.number
-    }
-  
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(request.params.id,
+       {name, number}, 
+       { new: true, runValidators: true, context: "query" })
       .then(updatedPerson => {
         response.json(updatedPerson)
       })
@@ -113,7 +116,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   }
-  else if (error.name === 'validationError') {
+  else if (error.name === 'ValidationError') {
     return response.status(400).json({error: error.message})
   }
 
